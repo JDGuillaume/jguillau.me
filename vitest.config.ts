609ca5177b5ea29ab6b4bb48b55b7dfb01c22221
Joey Guillaume <josephdguillaume@gmail.com>
2026-03-03 18:02:37 -0500

    chore(app): Manually implementing Cloudflare PR steps

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,5 +2,17 @@
 import { getViteConfig } from "astro/config";
 
 export default getViteConfig({
-  test: {},
+  test: {
+    coverage: {
+      provider: "v8",
+      thresholds: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80,
+      },
+    },
+    globals: true,
+    include: ["src/**/*.test.ts"],
+  },
 });
