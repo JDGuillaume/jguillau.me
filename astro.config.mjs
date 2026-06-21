f02702e8122dd907853de0631fdee23897b984e9
Joey Guillaume <josephdguillaume@gmail.com>
2026-06-21 15:47:26 -0400

    test(app): Using Node adapter conditionally for Vitest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
+import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import sugarcube from "@sugarcube-sh/vite";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig, fontProviders } from "astro/config";
 
+// Conditionally run Node adapter during Vitest testing (https://github.com/withastro/astro/issues/15878#issuecomment-4077891588).
+const adapter = process.env.VITEST
+  ? node({ mode: "standalone" })
+  : cloudflare({ imageService: "compile" });
+
 export default defineConfig({
-  adapter: cloudflare({
-    imageService: "compile",
-  }),
+  adapter,
   fonts: [
     {
       name: "Manrope",
